b5506c182ef852a29d61c0a2975ba79b00d326a1
VKalushev <vasko.kalushev@gmail.com>
2024-07-23 21:44:19 +0300

    Update

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-6xl">Home Page is Awesome!!!!!</h1>
+      <h1 className="text-6xl">Home Page is Awesome!</h1>
     </div>
   )
 }
