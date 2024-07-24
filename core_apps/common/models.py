1fb7a0032ffd93878fdf166149b1b601c1a37534
VKalushev <vasko.kalushev@gmail.com>
2024-07-24 21:48:13 +0300

    add TimeStamped, Profiles and Signals

--- core_apps/common/models.py
+++ core_apps/common/models.py
@@ -11,7 +11,7 @@ User = get_user_model()
 
 class TimeStampedModel(models.Model):
     pkid = models.BigAutoField(primary_key=True, editable=False)
-    id = models.UUIDField(default=uuid.uuid4, unique=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
