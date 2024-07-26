dcd02c5786bc8a383e914df2a59f7d239c948e4c
VKalushev <vasko.kalushev@gmail.com>
2024-07-26 12:11:35 +0300

    Add profile serializers and views

---
+++ core_apps/profiles/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import (AvatarUploadView, ProfileListAPIView, ProfileDetailAPIView,
+                    ProfileUpdateAPIView, NonTenantProfilesListAPIView)
+
+urlpatterns = [
+    path("all/", ProfileListAPIView.as_view(), name="profile-list"),
+    path("non-tenant-profiles/", NonTenantProfilesListAPIView.as_view(),
+         name="non-tenant-profiles"),
+    path("user/my-profile/", ProfileDetailAPIView.as_view(), name="profile-detail"),
+    path("user/update/", ProfileUpdateAPIView.as_view(), name="profile-update"),
+    path("user/avatar/", AvatarUploadView.as_view(), name="avatar-upload"),
+]
